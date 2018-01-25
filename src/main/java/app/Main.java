3828d7aa0f0e49462291d8ff02f3e1141a2554ff
Pahahentikys <Pavel1995.rus@yandex.ru>
2018-01-25 04:48:02 +0300

    1. В класс: EmployeeController добавлено логирование при помощи @Sl4g. 2. Из класса: Main выкинул мусор.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
 
-//        Flyway flyway = new Flyway();
-//        flyway.setDataSource("jdbc:h2:file:./src/main/resources/db/SpringBootTestDB", "sa", "");
-//        flyway.migrate();
-
         SpringApplication.run(Main.class, args);
     }
 }
--- src/main/java/app/controllers/EmployeeController.java
+++ src/main/java/app/controllers/EmployeeController.java
@@ -2,39 +2,46 @@ package app.controllers;
 
 import app.dom.Employee;
 import app.servImpl.EmployeeServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class EmployeeController {
 
     @Autowired
     private EmployeeServiceImpl employeeService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/employyes")
+    @RequestMapping(method = RequestMethod.GET, value = "/employees")
     public List<Employee> getAllEmlpoyyes() {
-            return employeeService.getAllEmlpoyyes();
+        log.info("Выполняется получение всех пользователей!");
+        return employeeService.getAllEmlpoyyes();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/employyes/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/employees/{id}")
     public Employee getEmployee(@PathVariable Long id){
+        log.info("Выполняется получение пользователя по id: {}", id);
         return employeeService.getEmployee(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/employyes")
-    public void addEmployee(@RequestBody Employee employee){
+    @RequestMapping(method = RequestMethod.POST, value = "/employees")
+    public void addEmployee(@RequestBody Employee employee) {
+        log.info("Выполняется добавление пользователя: {}", employee);
         employeeService.addEmployee(employee);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/employyes/{id}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/employees/{id}")
     public void updateEmployee(@RequestBody Employee employee, @PathVariable Long id){
+        log.info("Выполнено обновление данных о пользователе с id: {}. Новое имя: {},  новая фамилия: {}", id, employee.getName(), employee.getSurname());
         employeeService.updateEmployee(id, employee);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/employyes/{id}")
-    public void deleteEmployee(@PathVariable Long id){
+    @RequestMapping(method = RequestMethod.DELETE, value = "/employees/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        log.info("Выполняется удаление пользователя по id: {}", id);
         employeeService.deleteEmployee(id);
     }
 }
