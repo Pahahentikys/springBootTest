ddb948f345c12ccec937459e93c9046fa9bb5504
Pahahentikys <Pavel1995.rus@yandex.ru>
2018-01-25 04:49:39 +0300

    1. Из имплементации сервиса для работы с сотрудником убрал тренировочные примеры.

--- src/main/java/app/servImpl/EmployeeServiceImpl.java
+++ src/main/java/app/servImpl/EmployeeServiceImpl.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -17,44 +15,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-     private List<Employee> employees  = new ArrayList<>(
-Arrays.asList(
-                new Employee(1L, "Petr", "Sergeevich"),
-                new Employee(2L, "Petr", "Petrovich"),
-                new Employee(3L, "Petr", "Pavlovich")
-        )
-     );
-
     @Override
     public void deleteEmployee(Long id) {
-
-//        employees.removeIf(employee -> employee.getId().equals(id));
-
         employeeRepository.delete(id);
     }
 
     @Override
     public void updateEmployee(Long id, Employee employee) {
-//        for (int i = 0; i < employees.size(); i++) {
-//            Employee empl = employees.get(i);
-//            if(empl.getId().equals(id)){
-//                employees.set(i, employee);
-//                return;
-//            }
-//        }
-
-//        employees = employees.stream()
-//                 .map(e -> e.getId().equals(id) ? employee : e)
-//                 .collect(Collectors.toList());
-
         employeeRepository.save(employee);
     }
 
     @Override
-    public List<Employee> getAllEmlpoyyes(){
-
-//        return employees;
-
+    public List<Employee> getAllEmlpoyyes() {
         List<Employee> employees = new ArrayList<>();
         employeeRepository
                 .findAll()
@@ -64,19 +36,11 @@ Arrays.asList(
 
     @Override
     public void addEmployee(Employee employee) {
-//        employees.add(employee);
-
         employeeRepository.save(employee);
     }
 
     @Override
     public Employee getEmployee(Long id) {
-
-//       return employees.stream()
-//                .filter(employee -> employee.getId().equals(id))
-//                .findFirst()
-//                .get();
-
-       return employeeRepository.findOne(id);
+        return employeeRepository.findOne(id);
     }
 }
